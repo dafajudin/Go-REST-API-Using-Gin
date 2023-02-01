4ea533736609f91a20cdb0b93497d02b0f8dc7b6
Judin <dafajudin@student.telkomuniversity.ac.id>
2023-02-01 22:45:55 +0700

    Creating JWT Authentication Middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"go-api/utils/token"
+)
+
+func JwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := token.TokenValid(c)
+		if err != nil {
+			c.String(http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
